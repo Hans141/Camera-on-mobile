7433d078f51e5da6293ede722ccbe2d071ec0cf1
Hans141 <hans@techainer.com>
2022-03-23 00:49:13 +0700

    ahihi

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var videoElement = document.querySelector('video');
-var height =  {min: 720, ideal: 1080, max: 1440}
-var width =  {min: 1280, ideal: 1920, max: 2560}
+var height = { min: 200, ideal: 250, max: 400 }
+var width = { min: 250, ideal: 333, max: 500 }
 var videoSelect = document.querySelector('select#videoSource');
 
 let overlay = document.getElementById("card-overlayer")
@@ -21,7 +21,7 @@ function gotDevices(deviceInfos) {
   for (const deviceInfo of deviceInfos) {
     const option = document.createElement('option');
     option.value = deviceInfo.deviceId;
-   if (deviceInfo.kind === 'videoinput') {
+    if (deviceInfo.kind === 'videoinput') {
       option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
       videoSelect.appendChild(option);
     }
@@ -38,6 +38,10 @@ function getStream() {
   const constraints = {
     video: { deviceId: videoSource ? { exact: videoSource } : undefined }
   };
+  if (isMobile) {
+    constraints.video.width = width;
+    constraints.video.height = height;
+  }
   // console.log(constraints);
   let x = JSON.stringify(constraints);
   document.getElementsByClassName("noti")[0].textContent = x
@@ -50,19 +54,18 @@ function gotStream(stream) {
   videoSelect.selectedIndex = [...videoSelect.options].
     findIndex(option => option.text === stream.getVideoTracks()[0].label);
   videoElement.srcObject = stream;
-  videoElement.addEventListener('loadedmetadata', function(e){
-  console.log(videoElement.videoWidth, videoElement.videoHeight);
-  console.log(overlay)
-  overlay.style.width = videoElement.videoWidth + "px";
-  overlay.style.height = videoElement.videoHeight + "px";
-  if(isMobile) {
-  videoElement.style.height = "250px"
-  videoElement.style.width = "333px"
-  overlay.style.height = 250 + "px";
-  overlay.style.width = 333 + "px";
-
-  }
-});
+  videoElement.addEventListener('loadedmetadata', function (e) {
+    console.log(videoElement.videoWidth, videoElement.videoHeight);
+    console.log(overlay)
+    overlay.style.width = videoElement.videoWidth + "px";
+    overlay.style.height = videoElement.videoHeight + "px";
+    if (isMobile) {
+      videoElement.style.height = "250px"
+      videoElement.style.width = "333px"
+      overlay.style.height = 250 + "px";
+      overlay.style.width = 333 + "px";
+    }
+  });
 }
 
 function handleError(error) {
